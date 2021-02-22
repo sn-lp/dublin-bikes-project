79834f00de1bcb859683b26a18fbaea90567d455
sn-lp <sofia.pereira@ucdconnect.ie>
2021-02-22 14:33:48 +0000

    Adds database access details

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class Config:
     JCDECAUX_API_KEY = 'f5054ee21f6422a00c8247b74b556f10799ce4d4'
     # insert the database details here
-    DB_USER = ''
-    DB_PASSWORD = ''
-    DB_SERVER = 'your database endpoint'
-    DB_PORT = ''
+    DB_USER = 'admin'
+    DB_PASSWORD = 'stead49reply9077soil6025'
+    DB_SERVER = 'dublin-bikes-project.chdjwenxxv8q.us-east-1.rds.amazonaws.com'
+    DB_PORT = '3306'
     DB_NAME = 'dublin_bikes'
