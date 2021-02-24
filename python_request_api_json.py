f361d9f5a37a323e5792dc8e1da17cd999a36f54
rici-vio <71881288+rici-vio@users.noreply.github.com>
2021-02-24 20:14:08 +0000

    Dublin Bikes API Script (#11)
    * error handling and time
    
    * store json on s3 and boto3 exception handling
    
    * bug fix in config
    
    * typo fix
    
    * Cleanup after rebase
    
    Co-authored-by: reidy-p <paul-reidy@outlook.com>

--- python_request_api_json.py
+++ python_request_api_json.py
@@ -1,15 +1,32 @@
-# pip or pip3 install requests first
+import time
 import requests
+import boto3
+import json
+import datetime
 from config import Config
+
+
+s3 = boto3.client('s3')
 
 devConfig = Config()
 API_KEY = devConfig.JCDECAUX_API_KEY
+S3_BUCKET = devConfig.S3_BUCKET
 
-# get all stations in dublin 
-r = requests.get(f'https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey={API_KEY}')
-if r.status_code == requests.codes.ok:
-    stations_json = r.json()
-    
-    # stations_json is a list with dictionaires
-    for station in stations_json:
-        print("Station name: " + station['name'] + ", Available bikes: " + str(station['available_bikes']) + "\n")
+while True:
+    try:
+        # get all stations in dublin
+        r = requests.get(f'https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey={API_KEY}').json()
+
+        # store json on S3
+        s3.put_object(
+            Body=json.dumps(r),
+            Bucket=S3_BUCKET,
+            Key=f'dublin-bikes-{datetime.datetime.now()}.json'
+        )
+
+        # sleep for 5 minutes
+        time.sleep(5 * 60)
+
+    # boto3 exceptions
+    except Exception as e:
+        print ("Exception ", e)
