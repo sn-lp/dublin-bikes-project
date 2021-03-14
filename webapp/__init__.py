ebc97839a57c9964a0d7fbf3c0e4a3af730b9dce
rici-vio <71881288+rici-vio@users.noreply.github.com>
2021-03-14 20:38:52 +0000

    flask config

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config_dev import Config
 
 app = Flask(__name__)
-# loads the configuration from the config_dev module
+# loads the configuration from the config_dev module (default)
 app.config.from_object(Config)
 # overrides values with the contents of the file the CONFIG_SETTINGS environment variable points to
 app.config.from_envvar('CONFIG_SETTINGS')
