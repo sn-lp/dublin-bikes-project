c8940e5a347a5c744b12f4427ff1b40df810a433
sn-lp <sofia.pereira@ucdconnect.ie>
2021-03-28 23:16:16 +0100

    DB-77: creates a new predictions page that allows the user to select date, time and station to get the weather forecast and availability prediction

--- webapp/routes.py
+++ webapp/routes.py
@@ -5,12 +5,16 @@ from sqlalchemy import insert, create_engine
 from sqlalchemy.orm import sessionmaker
 import pandas as pd
 import sys
+from functools import lru_cache
+import requests
+import logging
 
 DB_USER = app.config["DB_USER"]
 DB_PASSWORD = app.config["DB_PASSWORD"]
 DB_SERVER = app.config["DB_SERVER"]
 DB_PORT = app.config["DB_PORT"]
 DB_NAME = app.config["DB_NAME"]
+OPENWEATHER_API_KEY = app.config["OPENWEATHER_API_KEY"]
 
 def get_station_availability_averages(station_property, station):
     # in mysql days of the week are represented by nnumbers from 0 to 6 (0 = Monday)
@@ -29,7 +33,7 @@ def index():
 
 @app.route("/predictions")
 def predictions():
-    return "This is our amazing prediction page"
+    return render_template('prediction.html')
 
 def connect_to_database():
     url = 'mysql+mysqlconnector://{}:{}@{}:{}/{}'.format(DB_USER, DB_PASSWORD, DB_SERVER, DB_PORT, DB_NAME)
@@ -52,6 +56,7 @@ def close_db_connection(exception):
         db.close()
 
 @app.route("/stations")
+@lru_cache
 def get_stations():
     stations = pd.read_sql("SELECT * FROM stations", g._database)
     availability = pd.read_sql("SELECT * FROM station_updates", g._database)
@@ -82,3 +87,46 @@ def get_station_available_spaces_history():
     if not station:
         abort(400)
     return get_station_availability_averages("freeStands", station)
+
+@app.route("/weather_forecast_and_availability_prediction")
+def get_weather_forecast_and_prediction_for_station():
+    station_latitude = request.args.get('stationLat')
+    station_longitude = request.args.get('stationLong')
+    time_requested_unix_timestamp = request.args.get('timeRequested')
+    if not station_latitude or not station_longitude or not time_requested_unix_timestamp:
+        abort(400)
+    # convert timestamp to int type again (when it is passed as a query parameter it is a string)
+    time_requested_unix_timestamp = int(time_requested_unix_timestamp)
+    # request weather forecast from openweather api
+    weather_forecast_api_response = requests.get(f'https://api.openweathermap.org/data/2.5/onecall?lat={station_latitude}&lon={station_longitude}&exclude=current,minutely,daily&units=metric&appid={OPENWEATHER_API_KEY}')
+    if weather_forecast_api_response.status_code == 200:
+        logging.info("Request to Openweather Forecast API succeeded with status code 200")
+        hourly_weather_forecast = weather_forecast_api_response.json()
+        if not 'hourly' in hourly_weather_forecast:
+            logging.error(f"OPENWEATHER Forecast API did not return expected data: {hourly_weather_forecast}.")
+            abort(400)
+        else:
+            for i in range(len(hourly_weather_forecast["hourly"])):
+                if hourly_weather_forecast["hourly"][i]["dt"] == time_requested_unix_timestamp:
+                    # get the weather variables from the api that correspond to the ones we are also storing in our (historical data) database
+                    main_weather = hourly_weather_forecast["hourly"][i]["weather"][0]["main"]
+                    temperature = hourly_weather_forecast["hourly"][i]["temp"]
+                    cloudiness = hourly_weather_forecast["hourly"][i]["clouds"]
+                    wind_speed = hourly_weather_forecast["hourly"][i]["wind_speed"]
+                    # get weather icon string from api that we can use to show the weather forecast to the user
+                    weather_icon = hourly_weather_forecast["hourly"][i]["weather"][0]["icon"]
+                    if not 'rain' in hourly_weather_forecast["hourly"][i]:
+                        rain = 0
+                    else:
+                        rain = hourly_weather_forecast["hourly"][i]["rain"]["1h"]
+                    if not 'snow' in hourly_weather_forecast["hourly"][i]:
+                        snow = 0
+                    else:
+                        snow = hourly_weather_forecast["hourly"][i]["snow"]["1h"]
+                    # for now return the weather forecast conditions to the frontend as a dictionary
+                    return {time_requested_unix_timestamp:{"main weather":main_weather, "temperature":temperature, "cloudiness": cloudiness, "wind speed": wind_speed, "rain": rain, "snow": snow, "icon": weather_icon}}
+            return {}    
+    else:
+        logging.error(f"Request to Openweather Forecast API failed with {weather_forecast_api_response.status_code}: {weather_forecast_api_response.reason}")
+        abort(400)
+    # TODO prediction part
