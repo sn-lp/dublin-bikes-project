024269c9c4c64307acefe417e098034c2872319a
Sofia Pereira <66690399+sn-lp@users.noreply.github.com>
2021-04-08 12:47:54 +0100

    DB-94: send the selected station's ID to the backend and transform user input data in the backend (#47)

--- webapp/routes.py
+++ webapp/routes.py
@@ -8,6 +8,11 @@ import sys
 from functools import lru_cache
 import requests
 import logging
+from datetime import datetime
+import calendar
+import math
+import pickle
+# from sklearn import the chosen type of model
 
 DB_USER = app.config["DB_USER"]
 DB_PASSWORD = app.config["DB_PASSWORD"]
@@ -26,6 +31,17 @@ def get_station_availability_averages(station_property, station):
         for row in rows:
             availability_history[day].append({f"{station_property}":int(row[0]), "hour": row[1]})
     return availability_history
+
+def get_day_of_the_week(user_timestamp_input):
+    dt_obj = datetime.fromtimestamp(user_timestamp_input)
+    dt_weekday_number = dt_obj.weekday()
+    dt_weekday_name = calendar.day_name[dt_weekday_number]
+    return dt_weekday_name
+
+def get_hour(user_timestamp_input):
+    dt_obj = datetime.fromtimestamp(user_timestamp_input)
+    dt_hour = dt_obj.hour
+    return dt_hour
 
 @app.route("/")
 def index():
@@ -90,13 +106,24 @@ def get_station_available_spaces_history():
 
 @app.route("/weather_forecast_and_availability_prediction")
 def get_weather_forecast_and_prediction_for_station():
+    station_number = request.args.get('stationNumber')
     station_latitude = request.args.get('stationLat')
     station_longitude = request.args.get('stationLong')
     time_requested_unix_timestamp = request.args.get('timeRequested')
-    if not station_latitude or not station_longitude or not time_requested_unix_timestamp:
+    if not station_latitude or not station_longitude or not time_requested_unix_timestamp or not station_number:
         abort(400)
-    # convert timestamp to int type again (when it is passed as a query parameter it is a string)
+
+    # convert timestamp and station_number to int type again (when it is passed as a query parameter it is a string)
     time_requested_unix_timestamp = int(time_requested_unix_timestamp)
+    station_number = int(station_number)
+    # get day of the week as a string to feed the prediction model 
+    day_of_the_week_string = get_day_of_the_week(time_requested_unix_timestamp)
+    # get hour as an int to get the sine and cosine
+    hour_of_the_day_int = get_hour(time_requested_unix_timestamp)
+    # get sine and cosine of the full hour to feed the prediction model
+    hour_sin = math.sin(2 * math.pi * hour_of_the_day_int/23.0)
+    hour_cos = math.cos(2 * math.pi * hour_of_the_day_int/23.0)
+
     # request weather forecast from openweather api
     weather_forecast_api_response = requests.get(f'https://api.openweathermap.org/data/2.5/onecall?lat={station_latitude}&lon={station_longitude}&exclude=current,minutely,daily&units=metric&appid={OPENWEATHER_API_KEY}')
     if weather_forecast_api_response.status_code == 200:
@@ -123,10 +150,24 @@ def get_weather_forecast_and_prediction_for_station():
                         snow = 0
                     else:
                         snow = hourly_weather_forecast["hourly"][i]["snow"]["1h"]
+                    
+                    # TODO load pickle file for corresponding station with the model object
+                    # station_model = pickle.load( open(f"station_model_{stationNumber}", "rb"))
+                    
+                    # TODO shape user and weather data to pass as argument to the predict method
+                    # might need to use this https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.DictVectorizer.html
+                    # user_features_and_values = {f'temperature': temperature, 'cloudiness': cloudiness, 'windSpeed': wind_speed,
+                                                #    'rain': rain, 'snow': snow, 'hour_sin': hour_sin, 'hour_cos': hour_cos,
+                                                #    'weekday_{day_of_the_week_string}': 1, 'mainWeather_{main_weather}': 1}
+
+                    # TODO predict availability for user input and weather forecast
+                    # availability_prediction = station_model.predict(user_features_and_values)
+
                     # for now return the weather forecast conditions to the frontend as a dictionary
+                    # later add the predicted availability to the dict
                     return {time_requested_unix_timestamp:{"main weather":main_weather, "temperature":temperature, "cloudiness": cloudiness, "wind speed": wind_speed, "rain": rain, "snow": snow, "icon": weather_icon}}
             return {"error": f"time requested didn't match available data: {hourly_weather_forecast['hourly']}"}    
     else:
         logging.error(f"Request to Openweather Forecast API failed with {weather_forecast_api_response.status_code}: {weather_forecast_api_response.reason}")
         abort(400)
-    # TODO prediction part
+
